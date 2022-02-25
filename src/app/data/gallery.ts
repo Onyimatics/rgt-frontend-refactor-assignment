c05ef78c7cf9baa7febab6dbfa6b5c8da978a616
Onyimatics <ezikeonyinyefavour@gmail.com>
2022-02-25 13:25:37 +0100

    separate data from logic and create services

---
+++ src/app/data/gallery.ts
@@ -0,0 +1,18 @@
+export const Galleries = [
+  {
+    img:"https://source.unsplash.com/1000x800",
+    desc:"No",
+  },
+  {
+    img:"https://source.unsplash.com/1000x802",
+    desc:"Repetitive",
+  },
+  {
+    img:"https://source.unsplash.com/1000x804",
+    desc:"Code",
+  },
+  {
+    img:"https://source.unsplash.com/1000x806",
+    desc:"Allowed",
+  }
+]
