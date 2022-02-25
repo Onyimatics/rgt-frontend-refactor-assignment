ce6aafef5a03588e34f38472c786151522b5ff5e
Onyimatics <ezikeonyinyefavour@gmail.com>
2022-02-25 11:05:15 +0100

    remove repetitive codes from html files

--- src/app/views/about/about.component.ts
+++ src/app/views/about/about.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  public companyMembers = [
+    {
+      img:"https://source.unsplash.com/200x200",
+      imgAlt:"Jane",
+      name:"Jane Doe",
+      title:"CEO & Founder",
+      desc:"Some text that describes me lorem ipsum ipsum lorem.",
+      email:"jane@example.com"
+    },
+    {
+      img:"https://source.unsplash.com/200x201",
+      imgAlt:"Mike",
+      name:"Mike Ross",
+      title:"Art Director",
+      desc:"Some text that describes me lorem ipsum ipsum lorem.",
+      email:"mike@example.com"
+    },
+    {
+      img:"https://source.unsplash.com/200x202",
+      imgAlt:"John",
+      name:"John Doe",
+      title:"Designer",
+      desc:"Some text that describes me lorem ipsum ipsum lorem.",
+      email:"john@example.com"
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
